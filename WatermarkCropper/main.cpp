940b8d9c2a569b702331b35073dfb414ecb505af
Serohyoon <78445463+vg16855@users.noreply.github.com>
2022-04-22 19:16:26 -0400

    Updated Resources and Fonts
    Uploaded fonts onto resources so they will stay when I move this to a new computer

--- WatermarkCropper/main.cpp
+++ WatermarkCropper/main.cpp
@@ -3,12 +3,16 @@
 #include <QApplication>
 #include <QCoreApplication>
 #include <QFile>
+#include <QFontDatabase>
 
 int main(int argc, char *argv[])
 {
     QApplication a(argc, argv);
 
     //set app style sheet
+    QFontDatabase::addApplicationFont(":/Resources/Lato-Regular.ttf");
+    QFontDatabase::addApplicationFont(":/Resources/BebasNeueu-Regular.ttf");
+
     QString stylePath = QCoreApplication::applicationDirPath();
     stylePath.append("/Diffnes.qss");
     QFile styleSheetFile(stylePath);
